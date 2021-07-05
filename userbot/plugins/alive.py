7eee0e89f7ff04ae970e7e9e34523d4f961359a1
zues <83701020+bhagath89@users.noreply.github.com>
2021-07-05 20:08:12 +0530

    Create alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -28,7 +28,7 @@ async def amireallyalive(alive):
         cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
         cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
         cat_caption += f"**{EMOJI} Master:** {mention}\n\n"
-        cat_caption += "   **[❣️DEPLOY DANGER_CAT❣️]**(https://github.com/dangerbots/danger_cat) "
+        cat_caption += "   **[❣️DEPLOY❣️]**(https://t.me/FlyingKILI) "
         await alive.client.send_file(
             alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
         )
@@ -43,7 +43,7 @@ async def amireallyalive(alive):
             f"**{EMOJI} Python Version :** `{python_version()}\n`"
             f"**{EMOJI} Uptime :** `{uptime}\n`"
             f"**{EMOJI} Master:** {mention}\n\n"
-                          "   **[❣️DEPLOY DANGER_CAT❣️]**(https://github.com/dangerbots/danger_cat) ",
+                          "   **[❣️DEPLOY❣️]**(https://t.me/FlyingKILI) ",
           )
      
 
